9198aaeb3ddcd67b3e734600da538d1d1b069966
HoLLy <dario.db@hotmail.com>
2017-12-22 15:21:35 +0100

    Add dnlib submodule, read all resource element names

---
+++ osu!ui skinner/OsuUIHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using dnlib.DotNet;
+using dnlib.DotNet.Resources;
+
+namespace osu_ui_skinner
+{
+    internal static class OsuUIHelper
+    {
+        public static void Extract(string fullPath)
+        {
+            var mod = ModuleDefMD.Load(fullPath);
+            var res = mod.Resources;
+
+            Debug.Assert(res.Count == 1);
+            Debug.Assert(res[0] is EmbeddedResource);
+            Debug.Assert(res[0].Name == "osu_ui.ResourcesStore.resources");
+
+            var r = (EmbeddedResource)res[0];
+
+            foreach (var element in ResourceReader.Read(mod, r.Data).ResourceElements) {
+                Console.WriteLine(element.Name);
+            }
+        }
+    }
+}
--- osu!ui skinner/Program.cs
+++ osu!ui skinner/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace osu_ui_skinner
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            PrintHeader();
+            
+            //clueless user ran our program, tell them no to
+            if (args.Length == 0) {
+                Console.WriteLine("This is a commandline application, but you can simply drag 'n' drop either an osu!ui.dll or extracted folder on me too, and I will work my magic!.");
+                Console.ReadLine();
+                return;
+            }
+
+            string path = Path.GetFullPath(args[0]);
+
+            if (Directory.Exists(path)) {
+                throw new NotImplementedException("Re-compiling osuui.dll is not yet implemented.");
+            } else if (File.Exists(path)) {
+                OsuUIHelper.Extract(path);
+            }
+            else {
+                Console.WriteLine("Please pass me an existing file or directory as parameter.");
+                return;
+            }
+        }
+
+        private static void PrintHeader()
+        {
+#if DEBUG
+            const string versionText = "(DEBUG BUILD)";
+#else
+            const string versionText = "v.indev";
+#endif
+            const string headerText = "osu!ui skinner" + " " + versionText;
+            int len = Math.Max(headerText.Length, 20);
+
+
+            Console.WriteLine("+" + new string('-', len + 2) + "+");
+            Console.WriteLine($"| {headerText.PadRight(len)} |");
+            Console.WriteLine("+" + new string('-', len + 2) + "+");
+            Console.WriteLine();
         }
     }
 }
